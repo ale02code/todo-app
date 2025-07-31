f62db495598aca96e00f9777c5a8900c6f48420d
Christopher <114704469+Blade-24@users.noreply.github.com>
2025-07-30 23:13:18 -0600

    finish the structure and design

---
+++ main.js
@@ -0,0 +1,10 @@
+const formTask = document.querySelector('#form_task');
+
+const inputTask = document.querySelector('#input_task');
+const btnAddTask = document.querySelector('#btn_add_task');
+
+const containerTasks = document.querySelector('#container_tasks');
+
+formTask.addEventListener('submit', (e) => {
+  e.preventDefault();
+})
