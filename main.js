e6d6a30bb08657a1b34619af19536a142c17e9c4
Christopher <114704469+Blade-24@users.noreply.github.com>
2025-07-31 19:26:32 -0600

    Update task input and button styles; enhance task list item layout

--- main.js
+++ main.js
@@ -5,6 +5,17 @@ const btnAddTask = document.querySelector('#btn_add_task');
 
 const containerTasks = document.querySelector('#container_tasks');
 
+let tasks = [];
+
+let task = {
+  id: '1',
+  title: inputTask,
+  completed: false
+}
+
 formTask.addEventListener('submit', (e) => {
   e.preventDefault();
-})
+  tasks.push(task);
+
+  console.log('Tareas pendientes:', tasks);
+});
