349fd9e303fc1fa27d074a773ed1807120474bf9
Christopher <114704469+Blade-24@users.noreply.github.com>
2025-07-31 20:01:22 -0600

    Refactor task rendering logic and clean up commented code

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const formTask = document.querySelector('#form_task');
 
 const inputTask = document.querySelector('#input_task');
@@ -5,17 +7,33 @@ const btnAddTask = document.querySelector('#btn_add_task');
 
 const containerTasks = document.querySelector('#container_tasks');
 
+let currentTask;
 let tasks = [];
 
-let task = {
-  id: '1',
-  title: inputTask,
-  completed: false
+inputTask.addEventListener('input', (e) => {
+  currentTask = e.target.value;
+});
+
+const renderTasks = (arr) => {
+  containerTasks.innerHTML = arr.map(task => `
+    <div class="w-full flex items-center gap-2 mb-2 overflow-hidden bg-indigo-300 rounded-sm p-2">
+      <li class="w-[calc(100%-50px)] break-words text-white">${task.title}</li>
+      <div class="w-[50px] h-10 flex items-center justify-center bg-indigo-600 rounded-sm py-1">
+        <img src="imgs/trash.png" alt="trash-icon" class="cursor-pointer ml-1 w-7 h-7">
+      </div>
+    </div>`).join('')
 }
 
 formTask.addEventListener('submit', (e) => {
   e.preventDefault();
-  tasks.push(task);
 
-  console.log('Tareas pendientes:', tasks);
+  let task = {
+    title: currentTask,
+  }
+
+  tasks.push(task);
+  inputTask.value = '';
+
+  renderTasks(tasks);
+  console.log('Tareas: ', tasks);
 });
